956690be0eeb9df773af044b0e9cb9a59618dc4b
Oliver-Qberry <162727806+Oliver-Qberry@users.noreply.github.com>
2025-05-12 11:47:37 -0700

    Update drive.cpp

--- kraken-template/src/kt/drive/drive.cpp
+++ kraken-template/src/kt/drive/drive.cpp
@@ -7,27 +7,30 @@
 #include "pros/rtos.hpp"
 #include <string>
 
-void kt::Chassis::initialize() {
+void kt::Chassis::initialize()
+{
     // resets imu and encoders
     imu.reset();
     reset_integrated_encoders();
 } // end of init function
 
 kt::Chassis::Chassis(std::vector<int> left_motor_ports, std::vector<int> right_motor_ports,
-                    pros::motor_brake_mode_e_t brake_type, 
-                    int imu_port, 
-                    double motor_rpm, double wheel_rpm, double wheel_diameter) : 
-                    imu(imu_port), 
-                    motor_rpm(motor_rpm), wheel_rpm(wheel_rpm), wheel_diameter(wheel_diameter) {
+                     pros::motor_brake_mode_e_t brake_type,
+                     int imu_port,
+                     double motor_rpm, double wheel_rpm, double wheel_diameter) : imu(imu_port),
+                                                                                  motor_rpm(motor_rpm), wheel_rpm(wheel_rpm), wheel_diameter(wheel_diameter)
+{
     // populate left motor vector
-    for (auto i : left_motor_ports) {
+    for (auto i : left_motor_ports)
+    {
         pros::Motor motor(abs(i));
         motor.set_reversed(kt::util::reversed_active(i));
         motor.set_brake_mode(brake_type);
         left_motors.push_back(motor);
     }
     // populate right motor vector
-    for (auto i : right_motor_ports) {
+    for (auto i : right_motor_ports)
+    {
         pros::Motor motor(abs(i));
         motor.set_reversed(kt::util::reversed_active(i));
         motor.set_brake_mode(brake_type);
@@ -35,100 +38,118 @@ kt::Chassis::Chassis(std::vector<int> left_motor_ports, std::vector<int> right_m
     }
 } // end of chassis constructor
 
-void kt::Chassis::opcontrol_tank() {
+void kt::Chassis::opcontrol_tank()
+{
     // set the analog channel ids for tank control
     _tank_drive = true;
     controller_x_id = pros::E_CONTROLLER_ANALOG_LEFT_Y;
     controller_y_id = pros::E_CONTROLLER_ANALOG_RIGHT_Y;
 }
 
-void kt::Chassis::opcontrol_arcade_standard() {
+void kt::Chassis::opcontrol_arcade_standard()
+{
     // set the analog channel ids for arcade control
     _tank_drive = false;
     controller_x_id = pros::E_CONTROLLER_ANALOG_LEFT_X;
     controller_y_id = pros::E_CONTROLLER_ANALOG_LEFT_Y;
 }
 
-void kt::Chassis::opcontrol_arcade_flipped() {
+void kt::Chassis::opcontrol_arcade_flipped()
+{
     // set the analog channel ids for arcade flipped control
     _tank_drive = false;
     controller_x_id = pros::E_CONTROLLER_ANALOG_RIGHT_X;
     controller_y_id = pros::E_CONTROLLER_ANALOG_RIGHT_Y;
 }
 
-void kt::Chassis::opcontrol_split_standard() {
+void kt::Chassis::opcontrol_split_standard()
+{
     // set the analog channel ids for split control
     _tank_drive = false;
     controller_x_id = pros::E_CONTROLLER_ANALOG_RIGHT_X;
     controller_y_id = pros::E_CONTROLLER_ANALOG_LEFT_Y; //
 }
 
-void kt::Chassis::opcontrol_split_flipped() {
+void kt::Chassis::opcontrol_split_flipped()
+{
     // set the analog channel ids for split flipped control
     _tank_drive = false;
     controller_x_id = pros::E_CONTROLLER_ANALOG_LEFT_X;
     controller_y_id = pros::E_CONTROLLER_ANALOG_RIGHT_Y;
 }
 
-void kt::Chassis::reset_integrated_encoders() {
+void kt::Chassis::reset_integrated_encoders()
+{
     // reset left motors
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         motor.set_zero_position(0);
     }
     // reset right motors
-    for (auto motor : right_motors) {
+    for (auto motor : right_motors)
+    {
         motor.set_zero_position(0);
     }
 }
 
-double kt::Chassis::get_average_integrated_encoders_positions() {
+double kt::Chassis::get_average_integrated_encoders_positions()
+{
     // sum of left
     double left_sum = 0;
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         left_sum += motor.get_position();
     }
     // avg of left motors
-    double left_avg = left_sum/left_motors.size();
+    double left_avg = left_sum / left_motors.size();
     // sum of right
     double right_sum = 0;
-    for(auto motor : right_motors) {
+    for (auto motor : right_motors)
+    {
         right_sum += motor.get_position();
     }
     // avg of right motors
-    double right_avg = right_sum/right_motors.size();
+    double right_avg = right_sum / right_motors.size();
     // return avg of the 2
-    return (left_avg + right_avg)/2;
+    return (left_avg + right_avg) / 2;
 }
 
-void kt::Chassis::brake() {
+void kt::Chassis::brake()
+{
     // brake left motors
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         motor.brake();
     }
     // brake right motors
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         motor.brake();
     }
 }
 
-void kt::Chassis::set_brake_modes(pros::motor_brake_mode_e_t brake) {
+void kt::Chassis::set_brake_modes(pros::motor_brake_mode_e_t brake)
+{
     // set brake for left motors
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         motor.set_brake_mode(brake);
     }
     // set brake for right motors
-    for (auto motor : right_motors) {
+    for (auto motor : right_motors)
+    {
         motor.set_brake_mode(brake);
     }
 }
 
-void kt::Chassis::move(double distance, double angle, double turn_multi) {
+void kt::Chassis::move(double distance, double angle, double turn_multi)
+{
     // reset drive motor encoders
     reset_integrated_encoders();
     // get the gearset ratio of the motor encoder
-    double gearset_rpm_ratio = ((50.0)/((motor_rpm)/(3600.0)))*((motor_rpm)/(wheel_rpm));
+    double gearset_rpm_ratio = ((50.0) / ((motor_rpm) / (3600.0))) * ((motor_rpm) / (wheel_rpm));
     // get the actual distance needed to travel in ticks
-    double distanceIn = (((gearset_rpm_ratio*distance)/wheel_diameter))/2;
+    double distanceIn = (((gearset_rpm_ratio * distance) / wheel_diameter)) / 2;
     // setup drive pid controller
     drive_pid_controller.reset();
     drive_pid_controller.set_goal(distanceIn);
@@ -140,15 +161,19 @@ void kt::Chassis::move(double distance, double angle, double turn_multi) {
     turn_pid_controller.set_goal(target_angle);
     double turn_error, turn_output;
     // if turn multi is 0 bypass the pid so goalmet will always be true
-    if (turn_multi == 0) {
+    if (turn_multi == 0)
+    {
         turn_pid_controller.bypass = true;
-    } else {
+    }
+    else
+    {
         turn_pid_controller.bypass = false;
     } // end of bypass if else
     // create direction integer variables
     int left_dir, right_dir;
     // pid loop
-    do {
+    do
+    {
         // find the current position with the motor encoders
         current_pos = get_average_integrated_encoders_positions();
         // get the drive pid output
@@ -161,11 +186,13 @@ void kt::Chassis::move(double distance, double angle, double turn_multi) {
         left_dir = (kt::util::sgn(kt::util::imu_error_calc(imu.get_heading(), target_angle)) > 0) ? -1 : 1;
         right_dir = (kt::util::sgn(kt::util::imu_error_calc(imu.get_heading(), target_angle)) > 0) ? 1 : -1;
         // set moves voltage to outputs
-        for (auto motor : left_motors) {
-            motor.move(drive_output + (turn_error*left_dir*turn_multi));
+        for (auto motor : left_motors)
+        {
+            motor.move(drive_output + (turn_error * left_dir * turn_multi));
         }
-        for (auto motor : right_motors) {
-            motor.move(drive_output + (turn_error*right_dir*turn_multi));
+        for (auto motor : right_motors)
+        {
+            motor.move(drive_output + (turn_error * right_dir * turn_multi));
         }
         // delay
         pros::delay(kt::util::DELAY_TIME);
@@ -173,41 +200,50 @@ void kt::Chassis::move(double distance, double angle, double turn_multi) {
     brake();
 } // end of move complex function
 
-void kt::Chassis::drive_pid_constants(double drive_kP, double drive_kI, double drive_kD, double drive_range) {
+void kt::Chassis::drive_pid_constants(double drive_kP, double drive_kI, double drive_kD, double drive_range)
+{
     // set pid const
     drive_pid_controller.set_pid_constants(drive_kP, drive_kI, drive_kD);
     drive_pid_controller.set_range(drive_range);
 }
 
-void kt::Chassis::turn_pid_constants(double turn_kP, double turn_kI, double turn_kD, double turn_range) {
+void kt::Chassis::turn_pid_constants(double turn_kP, double turn_kI, double turn_kD, double turn_range)
+{
     // set pid const
     turn_pid_controller.set_pid_constants(turn_kP, turn_kI, turn_kD);
     turn_pid_controller.set_range(turn_range);
 }
 
-void kt::Chassis::move(int voltage) {
+void kt::Chassis::move(int voltage)
+{
     // move left motors
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         motor.move(voltage);
     }
     // move right motors
-    for (auto motor : left_motors) {
+    for (auto motor : right_motors)
+    {
         motor.move(voltage);
     }
 }
 
-void kt::Chassis::turn(int voltage) {
+void kt::Chassis::turn(int voltage)
+{
     // move left motors
-    for (auto motor : left_motors) {
+    for (auto motor : left_motors)
+    {
         motor.move(voltage);
     }
     // move right motors in oposite direction
-    for (auto motor : left_motors) {
+    for (auto motor : right_motors)
+    {
         motor.move(voltage * -1);
     }
 }
 
-void kt::Chassis::opcontrol() {
+void kt::Chassis::opcontrol()
+{
     // get the current value from the analog channel (-127 to 127)
     int ana_x = master.get_analog(controller_x_id);
     int ana_y = master.get_analog(controller_y_id);
@@ -215,19 +251,26 @@ void kt::Chassis::opcontrol() {
     ana_x = (abs(ana_x) > JOYSTICK_X_THRESHOLD) ? ana_x : 0;
     ana_y = (abs(ana_y) > JOYSTICK_Y_THRESHOLD) ? ana_y : 0;
     // if in tank control mode
-    if (_tank_drive) {
-        for (auto motor : left_motors) {
-            motor.move(ana_x*JOYSTICK_X_SENSITIVITY);
+    if (_tank_drive)
+    {
+        for (auto motor : left_motors)
+        {
+            motor.move(ana_x * JOYSTICK_X_SENSITIVITY);
         }
-        for (auto motor : right_motors) {
-            motor.move(ana_y*JOYSTICK_Y_SENSITIVITY);
+        for (auto motor : right_motors)
+        {
+            motor.move(ana_y * JOYSTICK_Y_SENSITIVITY);
         }
-    } else { // if in any of the other control moves
-        for (auto motor : left_motors) {
-            motor.move(ana_y*JOYSTICK_Y_SENSITIVITY+ana_x*JOYSTICK_X_SENSITIVITY);
+    }
+    else
+    { // if in any of the other control moves
+        for (auto motor : left_motors)
+        {
+            motor.move(ana_y * JOYSTICK_Y_SENSITIVITY + ana_x * JOYSTICK_X_SENSITIVITY);
         }
-        for (auto motor : right_motors) {
-            motor.move(ana_y*JOYSTICK_Y_SENSITIVITY-ana_x*JOYSTICK_X_SENSITIVITY);
+        for (auto motor : right_motors)
+        {
+            motor.move(ana_y * JOYSTICK_Y_SENSITIVITY - ana_x * JOYSTICK_X_SENSITIVITY);
         }
         // if you are having issues with the direction of turning you can swap the plus and minus
     }
